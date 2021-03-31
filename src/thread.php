37782e4d66d6ed15d9229d30544590ee58a44887
Martijn <martijn@strocon.nl>
2021-03-31 08:17:43 +0200

    fix if the lib is in vendor directory

--- src/thread.php
+++ src/thread.php
@@ -1,17 +1,24 @@
 <?php
 use function Opis\Closure\{unserialize as u};
-require_once(dirname(__DIR__, 3).'/vendor/autoload.php');
+require_once(dirname(__DIR__, 4).'/vendor/autoload.php');
+\App\Http\Controllers\LogController::insertLog('thread.php pass 1');
+
 $jobId = $argv[1];
 $jobsDir = $argv[2];
 $logsDir = $argv[3];
 $helperClass = $argv[4];
 $helper = new $helperClass;
 $basePath = $helper->getAppBasePath();
-if(!file_exists("{$basePath}/{$jobsDir}/{$jobId}_closure.ser"))
+if(!file_exists("{$basePath}/{$jobsDir}/{$jobId}_closure.ser")) {
+    \App\Http\Controllers\LogController::insertLog('thread.php pass 2');
     die("Closure file for Job ID: $jobId doesn't exist");
-if(!file_exists("{$basePath}/{$jobsDir}/{$jobId}_arguments.ser"))
-	die("Arguments file for Job ID: $jobId doesn't exist");
+}
+if(!file_exists("{$basePath}/{$jobsDir}/{$jobId}_arguments.ser")) {
+    \App\Http\Controllers\LogController::insertLog('thread.php pass 3');
+    die("Arguments file for Job ID: $jobId doesn't exist");
+}
 try{
+    \App\Http\Controllers\LogController::insertLog('thread.php pass 4');
     $helper->bootstrap();
 	$wrapper = unserialize(file_get_contents("{$basePath}/{$jobsDir}/{$jobId}_closure.ser"));
 	$arguments = u(file_get_contents("{$basePath}/{$jobsDir}/{$jobId}_arguments.ser"));
